686c6b465615e9126cc55e3c8a399d62e4904405
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2019-09-10 12:29:09 +0530

    Added Frontend

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import numpy as np 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from sklearn.externals import joblib
 import praw 
 import re 
@@ -9,20 +9,17 @@ model = joblib.load("finalized_model.pkl")
 
 @app.route('/')
 def home():
-    message = "Flair Prediction Model As Microservice"
-    return jsonify(
-        response = message
-    )
+    return render_template('index.html')   
 
-@app.route('/predict', methods=['GET'])
+@app.route('/predict', methods=['POST'])
 def predict():
     reddit = praw.Reddit(client_id = '4olKQ-Up2BCKWQ', client_secret = 'sZsEDlw6NzhL3TpD95p1QMeH81E', user_agent = 'Sahil Gupta', username = 'the_stranded_alien', password = 'strandedalien')
-    if 'post' in request.args:
-        url = request.args['post']
-    else:
-        return "Error : No Post URL provided !"
+    features = [x for x in request.form.values()]
+    final_features = [np.array(features)]
+    url = final_features[0]
     u = str(url)
-    submission = reddit.submission(url=str(url))
+    u = u[2:-2]
+    submission = reddit.submission(url=u)
     submission.comments.replace_more(limit=None)
     comment = ''
     for top_comments in submission.comments:
@@ -50,10 +47,8 @@ def predict():
     feature = final_url + title + comment
     feature = np.array(feature).reshape((1,))
     prediction = model.predict(feature)
-    result = list(prediction)
-    return jsonify(
-        prediction = result
-    )
+    
+    return render_template('index.html', prediction_text='Flair : {}'.format(prediction[0]))
 
 if __name__ == "__main__":
     app.run(debug=True)
